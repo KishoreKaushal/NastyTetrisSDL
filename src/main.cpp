57b22986b640aa67db9d27e5996458f698fb451e
Kaushal Kishore <kshr4kshl@gmail.com>
2026-01-03 11:44:56 +0530

    linked all SDL libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+
 int main()
 {
     std::cout << "Hello World!" << std::endl;
